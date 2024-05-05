f597c7b3e810985ec447cc99ac11128bb9676185
Anton Kislyakov <antonkis2012@gmail.com>
2024-05-05 23:23:57 +0300

    Добавлен шейдер шерсти в проект, добавлен редактор шейдеров

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -121,6 +121,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TochpadMove
+        {
+            get
+            {
+                return SteamVR_Actions.default_TochpadMove;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
