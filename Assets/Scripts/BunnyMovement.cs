7553f155298b35568a19b1895a97bc740481eec1
Anton Kislyakov <antonkis2012@gmail.com>
2024-05-26 18:06:19 +0300

    Добавлена анимация шерстяного комка кролика в движении

--- Assets/Scripts/BunnyMovement.cs
+++ Assets/Scripts/BunnyMovement.cs
@@ -11,10 +11,18 @@ public class BunnyMovement : MonoBehaviour
     public GameObject clump;
 
     private GameObject[] objects;
+    private Animator animator;
+    private Rigidbody clumpRigidbody;
+    private bool isMoving = false;
+    private float speedThreshold = 0.25f;
+    private float minAnimationSpeed = 0.1f;
+    private float rotationSpeed = 5.0f;
 
     private void Start()
     {
         objects = new GameObject[] { ears, eyeballs, eyes, nose };
+        animator = GetComponent<Animator>();
+        clumpRigidbody = clump.GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -25,15 +33,36 @@ public class BunnyMovement : MonoBehaviour
             gameObject.transform.position = clump.transform.position;
 
             // Получаем направление, куда смотрит шарик (его скорость)
-            Vector3 ballDirection = clump.GetComponent<Rigidbody>().velocity.normalized;
+            Vector3 ballDirection = clumpRigidbody.velocity.normalized;
 
             // Находим угол между вектором направления шарика и осью Z
             float angle = Mathf.Atan2(ballDirection.x, ballDirection.z) * Mathf.Rad2Deg;
 
-            // Поворачиваем объект на этот угол, оставляя вращение по оси Y
-            gameObject.transform.rotation = Quaternion.Euler(-90, angle + 180f, 0);
+            // Рассчитываем целевую ротацию
+            Quaternion targetRotation = Quaternion.Euler(-90, angle + 180f, 0);
+
+            // Плавно поворачиваем объект к целевой ротации
+            gameObject.transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-       
+
+        // Проверяем скорость шарика и управляем анимациями
+        float speed = clumpRigidbody.velocity.magnitude;
+
+        // Устанавливаем скорость анимации, учитывая минимальную скорость
+        float animationSpeed = Mathf.Max(speed, minAnimationSpeed);
+        animator.SetFloat("Speed", animationSpeed);
+
+        if (speed > speedThreshold && !isMoving)
+        {
+            animator.ResetTrigger("StopGo");
+            animator.SetTrigger("StartGo");
+            isMoving = true;
+        }
+        else if (speed <= speedThreshold && isMoving)
+        {
+            animator.ResetTrigger("StartGo");
+            animator.SetTrigger("StopGo");
+            isMoving = false;
+        }
     }
 }
-
