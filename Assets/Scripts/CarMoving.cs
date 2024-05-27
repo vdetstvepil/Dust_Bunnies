b0a6a56f8095a5eb9b8e7d29c82444a5723799ea
Saiderany <alenapirogova2002@mail.ru>
2024-05-28 01:51:38 +0300

    Добавлены новые анимации. Добавлена логика управления машиной.

---
+++ Assets/Scripts/CarMoving.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+
+public class CarMoving : MonoBehaviour
+{
+    private Animator animator;
+    private SplineFollower splineFollower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        splineFollower = GetComponent<SplineFollower>();
+    }
+
+    public void CarMove()
+    {
+        if (animator != null)
+        {
+            animator.enabled = true;
+            animator.Play("rotation wheel");
+            splineFollower.enabled = true;
+        }
+    }
+}
