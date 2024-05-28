abe3d75fdc200193963e73a9f473dcf474486a60
Saiderany <alenapirogova2002@mail.ru>
2024-05-28 22:49:39 +0300

    создан предварительный префаб пылевого комка

---
+++ Assets/Scripts/KachelyController.cs
@@ -0,0 +1,23 @@
+using Dreamteck.Splines;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KachelyController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ToughtButtonKachely()
+    {
+        if (animator != null)
+        {
+            animator.enabled = true;
+            animator.Play("rotation_kachel");
+        }
+    }
+}
